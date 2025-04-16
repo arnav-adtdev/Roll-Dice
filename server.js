7d1a1f955378a8874916b49dfd0c830f6575cc50
Arnav <arnavchaturvedi0@gmail.com>
2025-04-16 22:01:26 +0530

    update

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.get('/', (req, res) => {
 
 // Connect to MongoDB with error handling
 mongoose.connect('mongodb://localhost:27017/diceGameDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
 }).then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
